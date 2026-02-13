1c4c5b61a8670882c63716fbf9a3bde18ad4ce20
tkoch97 <tkoch.dev@gmail.com>
2026-02-13 12:11:30 -0300

    refact: change  ResponseErrorJson of class to record

--- src/main/java/com/example/gerenciamento_usuarios/shared/exceptions/ResponseErrorJson.java
+++ src/main/java/com/example/gerenciamento_usuarios/shared/exceptions/ResponseErrorJson.java
@@ -1,21 +1,5 @@
 package com.example.gerenciamento_usuarios.shared.exceptions;
 
-import lombok.Getter;
-
-import java.util.List;
-
-@Getter
-public class ResponseErrorJson {
-    // getters
-    private String message;
-    private List<String> errors;
-
-    public ResponseErrorJson(String message) {
-        this.message = message;
-    }
-
-    public ResponseErrorJson(List<String> errors) {
-        this.errors = errors;
-    }
+public record ResponseErrorJson(String message) {
 }
 
